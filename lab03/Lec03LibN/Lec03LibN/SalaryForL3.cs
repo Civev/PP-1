fa657484cb9befd46e6fce40b8baeccfc1ee6cef
Civev <tguleshov@mail.ru>
2024-09-07 21:00:46 +0300

    added L3 Salary, lab is done

--- lab03/Lec03LibN/Lec03LibN/SalaryForL3.cs
+++ lab03/Lec03LibN/Lec03LibN/SalaryForL3.cs
@@ -6,21 +6,31 @@ using System.Threading.Tasks;
 
 namespace Lec03LibN
 {
-    internal class SalaryForL3 : IFactory
+    public class SalaryForL3 : IFactory
     {
+        
+        private float A { get; set; }
+
+        private float B { get; set; }
+        public SalaryForL3(float A, float B)
+        {
+           
+            this.A = A;
+            this.B = B;
+        }
         public IBonus GetA(float costOneHour)
         {
-            throw new NotImplementedException();
+            return new BonusAL3(costOneHour, A, B);
         }
 
         public IBonus GetB(float costOneHour, float x)
         {
-            throw new NotImplementedException();
+            return new BonusBL3(costOneHour, x, A, B);
         }
 
         public IBonus GetC(float costOneHour, float x, float y)
         {
-            throw new NotImplementedException();
+            return new BonusCL3(costOneHour,x,y, A, B);    
         }
     }
 }
