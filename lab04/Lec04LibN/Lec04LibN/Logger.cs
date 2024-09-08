b044f5dd4ad3c14254ae358fa7545031d412d6b1
Civev <tguleshov@mail.ru>
2024-09-08 23:16:46 +0300

    Added lab04

---
+++ lab04/Lec04LibN/Lec04LibN/Logger.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+
+namespace Lec04LibN
+{
+    public class Logger : ILogger
+    {
+        private Logger() { }
+
+        private static int indexes = 1;
+        
+        
+
+        private static string _logFormatTemplate = string.Format("-{0} {1}-", DateTime.Now.ToString("dd.MM.yyyy"), DateTime.Now.ToString("HH:mm:ss"));
+        private static Logger _instance;
+            static string _logFileName = string.Format(@"{0}/LOG{1}.txt", Directory.GetCurrentDirectory(), DateTime.Now.ToString("yyyyMMdd-HH-mm-ss"));
+        private List<string> _titles = new List<string>();
+        
+
+
+        public void log(string message)
+        {
+        
+
+            
+                indexes++;
+                string infoLog = string.Format("{0:d6}-{1}INFO ", indexes, _logFormatTemplate);
+                foreach (var item in _titles)
+                {
+                    infoLog += item;
+                }
+                Console.WriteLine($"{infoLog} {message}");
+            File.AppendAllText(_logFileName, $"{infoLog} {message}\n");
+
+
+        }
+        public static ILogger createLogger()
+        {
+            
+            
+                
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                    Console.WriteLine(string.Format("{0:d6}-{1}INIT", indexes, _logFormatTemplate));
+                File.AppendAllText(_logFileName, $"{string.Format("{0:d6}-{1}INIT", indexes, _logFormatTemplate)}\n");
+
+
+            }
+
+
+            return _instance;
+        }
+
+        public void start(string title)
+
+        {
+            
+             
+            
+            indexes++;
+            _titles.Add(title + ":");
+            string startLog = string.Format("{0:d6}{1}STRT ",indexes, _logFormatTemplate);
+            foreach (var item in _titles)
+            {
+                startLog += item;
+            }
+            File.AppendAllText(_logFileName, $"{startLog}\n");
+
+
+
+        }
+
+        public void stop()
+        {
+                
+            
+            indexes++;
+            if(_titles.Count != 1)
+                { 
+                _titles.RemoveAt(_titles.Count - 1); 
+                 }
+            string stopLog = string.Format("{0:d6}{1}STOP ", indexes, _logFormatTemplate);
+            foreach(var item in _titles)
+            {
+
+            stopLog += item; 
+            }
+            File.AppendAllText(_logFileName, $"{stopLog}\n");
+
+        }
+        
+     
+    }
+}
---
+++ lab04/Lec04LibN/PP-04/Program.cs
@@ -0,0 +1,19 @@
+﻿using Lec04LibN;
+
+ILogger logger = Logger.createLogger();
+
+logger.start("A");
+logger.log("333333");
+logger.start("B");
+
+logger.log("2123");
+
+
+ILogger logger2 = Logger.createLogger();
+logger.start("C");
+logger2.log("1111");
+logger2.stop();
+logger2.log("1111");
+logger2.stop();
+logger.stop();
+logger2.log("1111");
