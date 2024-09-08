1959c18d7f351f1e1ee6e87e2260bfe355530a6d
Civev <tguleshov@mail.ru>
2024-09-08 23:14:32 +0300

    Added lab01 in git

---
+++ lab01/lab01/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    internal class Car
+    {
+        public string model;
+        protected int startedAge;
+        private string radioPin;
+
+        public string Model { get
+            {
+                return model;
+            }
+            set
+            { model = value; }
+        }
+
+        public int StartedAge
+        {
+            get
+            {
+                return startedAge;
+            }
+            set
+            {
+                startedAge = value;
+            }
+        }
+
+        private string RadioPin
+        {
+            get
+            {
+                return radioPin;
+            }
+            set
+            {
+                radioPin = value;
+            }
+        }
+
+
+
+
+
+
+        public Car(string model, int age, string radioPin) { 
+            this.model = model;
+            this.startedAge = age;
+            this.radioPin = radioPin;
+        }
+        public Car() {
+            model = null;
+            startedAge = 0;
+            radioPin = null;
+        }
+
+        protected void EngenWasStarted()
+        {
+            Console.WriteLine("Двигатель запущен");
+        }
+        public bool OldCarOrNot()
+        {
+            return (startedAge < 2005);
+        }
+        private bool IsCarExisted()
+        {
+            if(model == null && radioPin == null && startedAge == 0)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ lab01/lab01/Truck.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    internal class Truck : Car
+    {
+        protected int weight;
+        protected int Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (weight > 0)
+                {
+                    weight = value;
+                }
+            }
+        }
+        public string firmName;
+        public string FirmName
+        {
+            get { return firmName; }
+            set
+            {
+                firmName = value;
+            }
+        }
+
+        private string tahometrNubmer;
+
+        public string TahometrNubmer
+        {
+            get { tahometrNubmer = firmName + weight.ToString();
+                return tahometrNubmer;
+               }
+            set { 
+                tahometrNubmer = value;
+            }
+        }
+
+        public Truck(string firmName, string tahometr, int weight) { 
+            this.firmName = firmName;
+            tahometrNubmer = tahometr;
+            this.weight = weight;
+            EngenWasStarted();
+        
+        }
+        public Truck() {
+            firmName = null;
+            tahometrNubmer = null;
+            weight = 0;
+        }
+
+        public void newDisctanation()
+        {
+            Console.WriteLine("Был построен новый маршрут");
+        }
+        private void GettedNewPappers(int numbers)
+        {
+            Console.WriteLine($"Были получены новые бумаги на погрузку, смотреть номер {numbers}");
+
+        }
+        protected string Break()
+        {
+            Console.WriteLine("Вам надо сделать перерыв, вы ехали: ");
+            return tahometrNubmer;
+        }
+
+    }
+}
