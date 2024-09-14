35a6cd361204a05fbf6a212c1078639ae6f4567c
Civev <tguleshov@mail.ru>
2024-09-14 21:54:34 +0300

    Adde lab06

--- lab03/Lec03LibN/PP03/obj/Debug/net8.0/PP03.AssemblyInfo.cs
+++ lab03/Lec03LibN/PP03/obj/Debug/net8.0/PP03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PP03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87a50bef0caca0d4c58dd9f4c1a6dbd5edb6254")]
 [assembly: System.Reflection.AssemblyProductAttribute("PP03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PP03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
