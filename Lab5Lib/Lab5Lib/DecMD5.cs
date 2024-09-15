7069001e1ef816d2d509f05af77d129a6935b977
Civev <tguleshov@mail.ru>
2024-09-15 20:55:36 +0300

    Added lab 05

---
+++ Lab5Lib/Lab5Lib/DecMD5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5Lib
+{
+    public class DecMD5 : Decorator
+    {
+        public DecMD5(IWriter? writer) : base(writer)
+        { }
+            public override string? Save(string? message)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var data = Encoding.UTF8.GetBytes(message ?? string.Empty);
+                var encryptData = md5.ComputeHash(data);
+                var hashedMessage = Convert.ToBase64String(encryptData);
+                var decoratedMessage = $"{message}{Constant.Delimitr}{hashedMessage}";
+                return writer?.Save(decoratedMessage);
+            }
+            }
+        
+        }
+    }
+
---
+++ Lab5Lib/Lab5Lib/DecRSA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5Lib
+{
+    public class DecRSA : Decorator
+    {
+        public DecRSA(IWriter? writer) : base(writer)
+        {
+        }
+        public override string? Save(string? message)
+        {
+            string publicKey;
+            byte[] encyptData;
+            message = message ?? string.Empty;
+            int index = message.IndexOf(Constant.Delimitr);
+            if (index == -1) throw new Exception("Delimitr not found");
+            string temp = message.Substring(0, index);
+            string temp2 = message.Substring(index + 1);
+            byte[] temp3 = Convert.FromBase64String(temp2);
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                publicKey = rsa.ToXmlString(true);
+                rsa.ImportParameters(rsa.ExportParameters(false));
+                encyptData = rsa.Encrypt(temp3, false);
+            }
+            string result = $"{temp}{Constant.Delimitr}{Convert.ToBase64String(encyptData)}{Constant.Delimitr}{publicKey}";
+            return writer?.Save(result);
+        }
+    }
+}
